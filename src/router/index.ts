e389c93082ddeb24467f9042969511e938a81f10
Kelu <keludev@gmail.com>
2021-07-28 23:50:33 +0200

    fix route

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,13 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: import(/* webpackChunkName: "about" */ '../views/GoogleMap.vue'),
+    component: () => import(/* webpackChunkName: "GoogleMap" */ '../views/GoogleMap.vue'),
+  },
+  {
+    // path: "*",
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: () => import(/* webpackChunkName: "GoogleMap" */ '../views/GoogleMap.vue'),
   },
 ];
 
