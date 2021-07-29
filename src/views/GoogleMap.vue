9a6a6b459de7bb4095dda2e7311b440e6f894d65
Kelu <keludev@gmail.com>
2021-07-29 21:44:16 +0200

    fixx css

--- src/views/GoogleMap.vue
+++ src/views/GoogleMap.vue
@@ -13,10 +13,10 @@
         </button>
       </div>
     </div>
-    <div class="col" :class="{'hide': fullPano}">
+    <div class="col-4" :class="{'hide': fullPano}">
       <div id="map" />
     </div>
-    <div class="col-8">
+    <div class="col">
       <div id="pano" />
     </div>
   </div>
@@ -139,22 +139,6 @@ export default defineComponent({
           latitude = pos.coords.latitude;
           longitude = pos.coords.longitude;
           centerMap(new google.maps.LatLng(latitude, longitude));
-
-          /* input = document.getElementById('pac-input') as HTMLInputElement;
-        if (input) {
-          const searchBox = new google.maps.places.SearchBox(input);
-          map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-          // Bias the SearchBox results towards current map's viewport.
-          map.addListener('bounds_changed', () => {
-            const bounds = map.getBounds();
-            if (bounds) {
-              searchBox.setBounds(bounds);
-            }
-          });
-        } */
-          /* setTimeout(() => {
-            getGeocode(new google.maps.LatLng(latitude, longitude));
-          }, 100); */
         }, () => { });
       }
     };
@@ -193,6 +177,7 @@ export default defineComponent({
         // eslint-disable-next-line no-empty
       } catch (err) { }
     });
+
     const changeFullStreet = () => {
       fullPano.value = !fullPano.value;
       setTimeout(() => {
@@ -214,54 +199,73 @@ export default defineComponent({
 <style>
   /* Always set the map height explicitly to define the size of the div
     * element that contains the map. */
-html, body {
-    height: 100%;
-    margin: 0;
-    padding: 0;
-  }
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
 
 #map {
   height: 100%;
   width: 100%;
 }
+
 #pano {
   height: 100%;
   width: 100%;
 }
-.column, .row{
+
+.column,
+.row {
   display: flex;
   flex-wrap: wrap;
 }
-.column{
+
+.column {
   flex-direction: column;
 }
-.column > .col{
+
+.column > .col {
   height: auto;
   max-height: 100%;
   min-height: 0;
   flex: 10000 1 0%;
 }
-.col-auto{
+
+.col-auto {
   flex: 0 0 auto !important;
 }
+
 .column > .col-8 {
-    height: 66.6667%;
-    width: auto;
+  height: 66.6667%;
+  width: auto;
 }
+
+.column > .col-4 {
+  height: 33.3333%;
+  width: auto;
+}
+
 .justify-center {
-    justify-content: center;
+  justify-content: center;
 }
-.ma-sm{
+
+.ma-sm {
   margin: 10px 15px;
 }
+
 .height-50p {
   height: 50%;
 }
+
 .full-screen {
   width: 100vw !important;
   height: 100vh !important;
 }
-.hide{
+
+.hide {
   display: none;
 }
+
 </style>
