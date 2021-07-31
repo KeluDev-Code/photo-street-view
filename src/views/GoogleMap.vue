aeb4ba1a894992cf44b017972a432ce97bcf82cc
Kelu <keludev@gmail.com>
2021-07-31 19:31:24 +0200

    remodelacion busqueda

--- src/views/GoogleMap.vue
+++ src/views/GoogleMap.vue
@@ -1,23 +1,29 @@
 <template>
-  <div class="column" style="width: 100%; height: 100vh;">
-    <div class="row justify-center col col-auto">
+  <div class="column" :class="{'fullPano': fullPano}" style="width: 100%; height: 100vh;"
+       @keyup.esc="changeFullStreet()"
+  >
+    <div class="row justify-center absolute-left">
       <div class="ma-sm">
-        <input v-model="inputUrl" type="text" placeholder="Search Box" @keyup.enter="processURL()">
+        <input v-model="inputUrl" type="text" placeholder="Search Box" style="height: 17px" @keyup.enter="processURL()">
         <button @click="processURL()">
-          Search
+          &#128269;
         </button>
-      </div>
-      <div class="ma-sm">
-        <button @click="changeFullStreet()">
-          Full Street View
+        <button @click="takeScreenShot()">
+          &#128248;
         </button>
       </div>
     </div>
     <div class="col-4" :class="{'hide': fullPano}">
       <div id="map" />
     </div>
-    <div class="col">
+    <div class="col relative">
+      <div class="btn-absolute">
+        <button style="font-weight: bold; font-size: 16px;" @click="changeFullStreet()">
+          {{ fullPano ? '&#709;' : '&#708;' }}
+        </button>
+      </div>
       <div id="pano" />
+      <!-- https://html2canvas.hertzen.com/ -->
     </div>
   </div>
 </template>
@@ -26,6 +32,7 @@
 import {
   defineComponent, onUnmounted, onMounted, ref,
 } from 'vue';
+import html2canvas from 'html2canvas';
 
 declare global {
     interface Window { initAutocomplete: ()=> void }
@@ -45,8 +52,8 @@ export default defineComponent({
     const googleApiKey = process.env.VUE_APP_GOOGLE_MAPS_API_KEY;
 
     // Manchester
-    let latitude = 53.4754446;
-    let longitude = -2.2615203;
+    let latitude = 53.4730789;
+    let longitude = -2.2482232;
 
     let { google } = window;
     let map: google.maps.Map;
@@ -84,6 +91,7 @@ export default defineComponent({
       }));
     };
 
+    // TODO: buscar lloc
     const processURL = () => {
       const uri = decodeURIComponent(inputUrl.value);
       const match = uri.match('^https://.*!1s(.*)!2e.*$');
@@ -143,6 +151,91 @@ export default defineComponent({
       }
     };
 
+    const changeFullStreet = () => {
+      fullPano.value = !fullPano.value;
+      setTimeout(() => {
+        google.maps.event.trigger(panorama, 'resize');
+      }, 100);
+    };
+    // https://www.aspsnippets.com/Articles/Take-Screenshot-Snapshot-of-Google-Maps-using-JavaScript.aspx
+    const takeScreenShot = () => {
+      // const canvaStreeView = document.getElementsByClassName('widget-scene-canvas');
+      const canvaStreeView = document.getElementById('pano');
+      if (canvaStreeView) {
+        html2canvas(canvaStreeView, {
+          useCORS: true,
+        }).then((canvas) => {
+          const link = document.createElement('a');
+
+          if (typeof link.download === 'string') {
+            link.href = canvas.toDataURL();
+            link.download = 'Capture.png';
+
+            // Firefox requires the link to be in the body
+            document.body.appendChild(link);
+
+            // simulate click
+            link.click();
+
+            // remove the link when done
+            document.body.removeChild(link);
+          } else {
+            window.open(canvas.toDataURL());
+          }
+        });
+        /* const newCanvas = document.createElement('canvas');
+        newCanvas.id = 'screenShot';
+        const context = newCanvas.getContext('2d');
+
+        // set dimensions
+        newCanvas.width = (canvaStreeView[0] as HTMLCanvasElement).width;
+        newCanvas.height = (canvaStreeView[0] as HTMLCanvasElement).height;
+
+        // apply the old canvas to the new one
+        context!.drawImage(canvaStreeView[0] as HTMLCanvasElement, 0, 0);
+        // const canvas = canvaStreeView[0].cloneNode(true);
+        // console.log(canvas);
+        // (canvas as Element).className = '';
+
+        document.body.appendChild(newCanvas);
+        /// html2canvas(body[0]).then((canvas) => {
+        /* (canvaStreeView[0] as HTMLCanvasElement).toBlob((blob) => {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const file: any = blob;
+            file.lastModifiedDate = new Date();
+            file.name = 'Capture';
+          }); *
+        const fileName = 'Capture';
+        const blob = (newCanvas as HTMLCanvasElement).toDataURL();
+        if (blob) {
+          try {
+            if (window.navigator.msSaveOrOpenBlob) {
+              window.navigator.msSaveOrOpenBlob(blob, `${fileName}`);
+            } else {
+              const downloadUrl = blob;
+              const link = window.document.createElement('a');
+              // safari doesn't support this yet
+              if (typeof link.download === 'undefined') {
+                window.location.href = downloadUrl;
+              } else {
+                link.href = downloadUrl;
+                link.setAttribute('download', `${fileName}.png`);
+                window.document.body.appendChild(link);
+                link.click();
+              }
+
+              try {
+                setTimeout(() => { window.document.body.removeChild(link); }, 200);
+                // eslint-disable-next-line no-empty
+              } catch (err) { }
+            }
+            // eslint-disable-next-line no-empty
+          } catch (ex) { }
+        } */
+        // });
+      }
+    };
+
     window.initAutocomplete = () => {
       initMap();
     };
@@ -178,18 +271,12 @@ export default defineComponent({
       } catch (err) { }
     });
 
-    const changeFullStreet = () => {
-      fullPano.value = !fullPano.value;
-      setTimeout(() => {
-        google.maps.event.trigger(panorama, 'resize');
-      }, 100);
-    };
-
     return {
       inputUrl,
       processURL,
       fullPano,
       changeFullStreet,
+      takeScreenShot,
     };
   },
 });
@@ -245,6 +332,32 @@ body {
 .column > .col-4 {
   height: 33.3333%;
   width: auto;
+}
+
+.relative {
+  position: relative;
+}
+
+.absolute-left {
+  position: absolute;
+  top: 15px;
+  left: 10px;
+  z-index: 10;
+}
+
+.btn-absolute {
+  position: absolute;
+  top: -12px;
+  left: 50%;
+  z-index: 10;
+}
+
+.btn-absolute button {
+  width: 50px;
+}
+
+.fullPano .btn-absolute {
+  top: 1px;
 }
 
 .justify-center {
